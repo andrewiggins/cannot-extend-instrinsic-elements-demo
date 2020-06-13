df7a387a67d2e1673c97eabb857f5c6cbdfd5a67
Andre Wiggins <andrewiggins@live.com>
2020-06-13 14:36:45 -0700

    Update JSX extending with pattern that works back to TS 3.5.3

--- src/global.d.ts
+++ src/global.d.ts
@@ -14,8 +14,18 @@
 //     }
 // }
 
-// Works with TypeScript 3.9.1-rc but not 3.9.0-beta
-declare namespace preact.JSX {
+// // Works with TypeScript 3.9.1-rc but not 3.9.0-beta
+// declare namespace preact.JSX {
+//   interface IntrinsicElements {
+//     "global-custom-element": {
+//       attr: number;
+//       attr2: SomeCustomInterfaceToDetermineWhetherThisFileIsBeingIncluded;
+//     };
+//   }
+// }
+
+// Works with TypeScript 3.6.5, 3.7.5, 3.8.3, 3.9.0-beta, 3.9.5
+declare namespace preact.createElement.JSX {
   interface IntrinsicElements {
     "global-custom-element": {
       attr: number;
--- src/module.tsx
+++ src/module.tsx
@@ -11,9 +11,18 @@ export interface ModuleCEAttributes extends preact.JSX.HTMLAttributes {
   onsomeevent?: (this: ModuleCE, ev: ModuleCEEvent) => void;
 }
 
-// Works with TypeScript 3.9.1-rc but not 3.9.0-beta
+// // Works with TypeScript 3.9.1-rc but not 3.9.0-beta
+// declare module "preact" {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       "module-custom-element": ModuleCEAttributes;
+//     }
+//   }
+// }
+
+// Works with TypeScript 3.6.5, 3.7.5, 3.8.3, 3.9.0-beta, 3.9.5
 declare module "preact" {
-  namespace JSX {
+  namespace createElement.JSX {
     interface IntrinsicElements {
       "module-custom-element": ModuleCEAttributes;
     }
